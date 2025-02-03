900f9484c8b8db42360a0309f87c8800caceb13e
shreyavats <shreyavats2407@gmail.com>
2025-02-03 14:34:42 +0530

    new changes

--- models/userSchema.js
+++ models/userSchema.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 mongoose
-  .connect("mongodb://localhost:27017/TodoDashboard")
+  .connect(process.env.DB_URL)
   .then(() => {
     console.log("mongoDB connected");
   })
