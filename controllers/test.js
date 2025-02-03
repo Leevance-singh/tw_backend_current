2707a19b2471243e9fde7b06a910df1ee16affcd
shreyavats <shreyavats2407@gmail.com>
2025-02-03 11:45:35 +0530

    test

---
+++ controllers/test.js
@@ -0,0 +1,6 @@
+const sample = async (req,res)=>{
+    res.send("Hello From Route!!");
+}
+module.exports={
+    sample
+}
--- routes/todoRouter.js
+++ routes/todoRouter.js
@@ -5,9 +5,10 @@ const router = express.Router();
 const authController = require("../controllers/AuthController");
 const taskController = require("../controllers/taskController");
 const {verifyUser} = require("../middlewares/Authenticate");
+const {sample} = require("../controllers/test");
 
 
-router.get('/',test);
+router.get('/',sample);
 router.get('/login',authController.loginUser);
 router.post("/addTask",verifyUser,taskController.addTask);
 router.post("/addSection",verifyUser,taskController.addNewSection)
