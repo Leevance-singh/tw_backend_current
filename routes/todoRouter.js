565794ea733490c77f95e0fe0802f1eeeb1055bb
shreyavats <shreyavats2407@gmail.com>
2025-02-03 14:25:26 +0530

    new changes

--- routes/todoRouter.js
+++ routes/todoRouter.js
@@ -4,15 +4,14 @@ const router = express.Router();
 
 const authController = require("../controllers/AuthController");
 const taskController = require("../controllers/taskController");
-const {verifyUser} = require("../middlewares/Authenticate");
-const {sample} = require("../controllers/test");
+const authMiddleware = require("../middlewares/Authenticate");
 
 
-router.get('/',sample);
+router.get('/auth/checkToken',authMiddleware.checkLoginStatus);
 router.post("/verifyEmail",authController.verifyEmail);
 router.post("/signup",authController.signupUser);
 router.get('/login',authController.loginUser);
-router.post("/addTask",verifyUser,taskController.addTask);
-router.post("/addSection",verifyUser,taskController.addNewSection);
+router.post("/addTask",authMiddleware.verifyUser,taskController.addTask);
+router.post("/addSection",authMiddleware.verifyUser,taskController.addNewSection);
 
 module.exports =router
